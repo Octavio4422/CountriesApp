c973133c799c09c830ebd678f5c642f84b87b702
Octavio4422 <octaviopaz02@gmail.com>
2022-05-07 00:08:06 -0300

    express update

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+const countriesRoute = require('./countries');
+const activityRoute = require('./activity');
+
 
 
 const router = Router();
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+router.use('/countries', countriesRoute);
+router.use('/activity', activityRoute);
 
 
 module.exports = router;

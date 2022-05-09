f398996fb41b076c42cfd884cbfa099cd078a672
Octavio4422 <octaviopaz02@gmail.com>
2022-05-09 13:54:12 -0300

    fixed route

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -3,36 +3,35 @@ const { Country } = require("../db");
 const router = Router();
 const axios = require("axios");
 
-router.get("/", async(req, res, next) => {
-  // const { name } = req.query;
-  const response = await apiToDb();
-  try {
-    res.json(response);
-  } catch (e) {
-    next(e);
-  }
-});
 
-async function apiToDb(){
+router.get("/", apiToDb);
+
+async function apiToDb(req, res, next){
+  
+  const verifyDb = await Country.findAll();
+  if(verifyDb.length){
+    return res.status(200).json(verifyDb);
+  }
+  
   let fetchCountries = await axios.get("https://restcountries.com/v3/all");
 
   let allCountries = fetchCountries.data;
 
-  allCountries.forEach((c) => {
-    Country.create({
-      id: c.cca3,
-      name: c.name.common,
-      flags: c.flags[0],
-      region: c.region,
-      capital: c.capital ? c.capital[0] : "Doesnt have",
-      subregion: c.subregion,
-      area: c.area,
-      population: c.population,
-    });
+  for(let i = 0; i < allCountries.length; i++){
+    await Country.create({
+      id: allCountries[i].cca3,
+          name: allCountries[i].name.common,
+          flags: allCountries[i].flags[0],
+          region: allCountries[i].region,
+          capital: allCountries[i].capital ? allCountries[i].capital[0] : "Doesnt have",
+          subregion: allCountries[i].subregion,
+          area: allCountries[i].area,
+          population: allCountries[i].population,
+    })
+  }
 
-    const response = await Country.findAll();
-    console.log(response)
-  });
+  const response = await Country.findAll(); 
+  return res.status(200).json(response)
 };
 
 module.exports = router;

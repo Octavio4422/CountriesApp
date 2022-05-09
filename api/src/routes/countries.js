8d35b9b3d22b3c69bf030fa54c6b0824174b4c89
Octavio4422 <octaviopaz02@gmail.com>
2022-05-09 15:27:59 -0300

    update

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,37 +1,50 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const { Country } = require("../db");
 const router = Router();
 const axios = require("axios");
 
+router.get("/", routerFunction);
 
-router.get("/", apiToDb);
-
-async function apiToDb(req, res, next){
+function routerFunction(req, res, next) {
+  const { name } = req.query
   
-  const verifyDb = await Country.findAll();
-  if(verifyDb.length){
-    return res.status(200).json(verifyDb);
+  if(!name){
+    apiToDb().then((response) => {
+      res.status(200).json(response);
+    });
   }
-  
+
+  if(name){
+    
+    res.status(200).json('bueno todo anda bien')
+  }
+}
+
+async function apiToDb() {
+  const verifyDb = await Country.findAll();
+  if (verifyDb.length) {
+    return verifyDb;
+  }
+
   let fetchCountries = await axios.get("https://restcountries.com/v3/all");
 
   let allCountries = fetchCountries.data;
 
-  for(let i = 0; i < allCountries.length; i++){
+  for (let i = 0; i < allCountries.length; i++) {
     await Country.create({
       id: allCountries[i].cca3,
-          name: allCountries[i].name.common,
-          flags: allCountries[i].flags[0],
-          region: allCountries[i].region,
-          capital: allCountries[i].capital ? allCountries[i].capital[0] : "Doesnt have",
-          subregion: allCountries[i].subregion,
-          area: allCountries[i].area,
-          population: allCountries[i].population,
-    })
+      name: allCountries[i].name.common,
+      flags: allCountries[i].flags[0],
+      region: allCountries[i].region,
+      capital: allCountries[i].capital? allCountries[i].capital[0]: "Doesnt have",
+      subregion: allCountries[i].subregion,
+      area: allCountries[i].area,
+      population: allCountries[i].population,
+    });
   }
 
-  const response = await Country.findAll(); 
-  return res.status(200).json(response)
-};
+  const response = await Country.findAll();
+  return response;
+}
 
 module.exports = router;

1cdffb223e1eebbf23e23a2bb8c57ac20f2d6bf2
Octavio4422 <octaviopaz02@gmail.com>
2022-05-10 18:02:57 -0300

    update models

--- api/src/models/TouristActivity.js
+++ api/src/models/TouristActivity.js
@@ -1,23 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('touristActivity', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    difficulty:{
-        type: DataTypes.INTEGER,
-    },
-    duration:{
-        type: DataTypes.INTEGER,
-    },
-    season:{
+  sequelize.define(
+    "touristActivity",
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
         type: DataTypes.STRING,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+      },
+      season: {
+        type: DataTypes.STRING,
+      },
     },
-  },{
-    timestamps: false,
-  });
+    {
+      timestamps: false,
+    }
+  );
 };

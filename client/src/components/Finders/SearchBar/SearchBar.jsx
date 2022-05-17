e089f7e1cfd16518e783eef021c023124b7abcb2
Octavio Paz <octaviopaz02@gmail.com>
2022-05-17 02:10:34 -0300

    update

---
+++ client/src/components/Finders/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { queryCountries } from '../../../redux/actions';
+
+export default function SeachBar(){
+    let dispatch = useDispatch();
+    
+    const [input, setInput] = useState("")
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        dispatch(queryCountries(input))
+    }
+    
+    return(
+        <div>
+            <form  onSubmit={(e) => handleSumbit(e)} >
+                <input 
+                    type={'text'} 
+                    placeholder='Search Countries'
+                    onChange={(e) => setInput(e.target.value)}    
+                    />
+            </form>
+        </div>
+    )
+}
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Countries from "../Countries/Countries";
+import SeachBar from "../Finders/SearchBar/SearchBar";
+
+export default function Home() {
+  return (
+    <div>
+      <SeachBar />
+      <Countries />
+    </div>
+  );
+}

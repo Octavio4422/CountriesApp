95abce9ca9b6c742d7a4c39e2e045ce0066129c3
Octavio Paz <octaviopaz02@gmail.com>
2022-05-17 02:11:15 -0300

    update

--- client/src/components/Country/Country.jsx
+++ client/src/components/Country/Country.jsx
@@ -3,8 +3,8 @@ import styles from './Country.module.css'
 
 export default function Country({id, name, flags, region, subregion, area, population}){
     return <div className={styles.Card}>
-        <img className={styles.Image} src={flags} alt="imagen"  />
         <h2>{name}</h2>
-        <h3>{region}</h3>
+        <img className={styles.Image} src={flags} alt="imagen"  />
+        <h4 className={styles.Continent} >{region}</h4>
     </div>
 }

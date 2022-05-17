7b95187dab912aae8505479a9d319c4ef866ff93
Octavio Paz <octaviopaz02@gmail.com>
2022-05-17 02:11:58 -0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import {Routes, Route} from "react-router-dom";
 import "./App.css";
-import Countries from "./components/Countries/Countries";
+// import Countries from "./components/Countries/Countries";
 import NavBar from "./components/NavBar/NavBar"
+import Home from "./components/Home/Home"
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Routes>
-        <Route exact path='/' element={<Countries/>} />
+        <Route exact path='/' element={<Home/>} />
       </Routes>
     </div>
   );
